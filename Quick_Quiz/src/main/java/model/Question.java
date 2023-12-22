25f97fae1dc8bc11c2b7070a19db974dab62188d
Naoshin Anzum Hridi <naoshinhridi@gmail.com>
2023-12-22 23:05:13 +0600

    update questions.json

--- Quick_Quiz/src/main/java/model/Question.java
+++ Quick_Quiz/src/main/java/model/Question.java
@@ -15,25 +15,11 @@ public class Question {
         return question;
     }
 
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
     public String[] getOptions() {
         return options;
-    }
-
-    public void setOptions(String[] options) {
-        this.options = options;
     }
 
     public int getAnswerKey() {
         return answerKey;
     }
-
-    public void setAnswerKey(int answerKey) {
-        this.answerKey = answerKey;
-    }
-
-    // Constructors, getters, and setters
 }
